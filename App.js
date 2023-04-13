e1a2fca2aab4afa4c9261e990e1acb73293eb0df
SamaraSilvia81 <samarasilvia3110@gmail.com>
2023-04-12 21:14:06 -0300

    Deleted PermissionCamera

--- App.js
+++ App.js
@@ -2,47 +2,16 @@ import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Torch from 'react-native-torch';
 import RNShake from 'react-native-shake';
-import {PermissionsAndroid} from 'react-native';
 
 const App = () => {
   
   const [toggle, setToggle] = useState(false);
-  const [hasPermission, setHasPermission] = useState(false);
-
+  
   const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
 
   useEffect(() => {
-    const requestPermission = async () => {
-      if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Permissão de câmera',
-            message: 'Este aplicativo precisa de acesso à câmera para usar a lanterna.',
-            buttonNeutral: 'Perguntar depois',
-            buttonNegative: 'Cancelar',
-            buttonPositive: 'OK',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          setHasPermission(true);
-        } else {
-          setHasPermission(false);
-        }
-      } catch (err) {
-        console.warn(err);
-      }
-    }
-    };
-    requestPermission();
-  }, []);
-
-  useEffect(() => {
-    if (hasPermission) {
-      Torch.switchState(toggle);
-    }
-  }, [toggle, hasPermission]);
+    Torch.switchState(toggle);
+  }, [toggle]);
 
   useEffect(() => {
     /**
